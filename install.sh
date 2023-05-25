c1331d579bf2cffef050551bd5db43db11b69e77
Josh Harshman <josh.harshman@bit.ly>
2023-05-25 11:49:54 -0600

    bump version in install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Installs FWSYNC release.
 
-VERSION="v0.0.1-2"
+VERSION="v0.0.1-3"
 OS=$(uname -s | tr -d '\n')
 ARCH=$(uname -m | tr -d '\n')
 RELEASE=https://github.com/jharshman/fwsync/releases/download/${VERSION}/fwsync_${OS}_${ARCH}.tar.gz
